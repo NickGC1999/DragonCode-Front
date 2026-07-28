e6725debf8c3d3128e72ed96bb4f8a964dc7b71f
DNCGuanoluisa <danielguanoluisa23@gmail.com>
2026-07-27 21:10:46 -0500

    adiccion de botones para volver

--- src/app/crear-cuenta/crear-cuenta.component.ts
+++ src/app/crear-cuenta/crear-cuenta.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-crear-cuenta',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './crear-cuenta.component.html',
   styleUrl: './crear-cuenta.component.scss'
 })
-export class CrearCuentaComponent {
-
-}
+export class CrearCuentaComponent {}
--- src/app/recuperar-cuenta/recuperar-cuenta.component.ts
+++ src/app/recuperar-cuenta/recuperar-cuenta.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { CyberLayoutComponent } from '../cyber-layout/cyber-layout.component';
 
 @Component({
   selector: 'app-recuperar-cuenta',
   standalone: true,
-  imports: [CyberLayoutComponent], // El cascarón que envuelve al formulario
+  imports: [CyberLayoutComponent, RouterLink],
   templateUrl: './recuperar-cuenta.component.html',
   styleUrl: './recuperar-cuenta.component.scss'
 })
 export class RecuperarCuentaComponent {}
+
